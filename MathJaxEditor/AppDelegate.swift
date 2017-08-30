87a386dd932a41f3065169dc28505cb22ffa0b7f
Orlando, Ding <or.ding@sap.com>
2017-08-30 15:22:39 +0800

    Initial Commit

---
+++ MathJaxEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MathJaxEditor
+//
+//  Created by Ding, Orlando on 30/08/2017.
+//  Copyright © 2017 Ding, Orlando. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MathJaxEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MathJaxEditor
+//
+//  Created by Ding, Orlando on 30/08/2017.
+//  Copyright © 2017 Ding, Orlando. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
