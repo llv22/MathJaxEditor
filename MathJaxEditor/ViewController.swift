2511d2a628fca96dfc02526121ec47abaefccf36
Orlando, Ding <or.ding@sap.com>
2017-08-30 16:39:08 +0800

    i058959 - for local running editor

--- MathJaxEditor/ViewController.swift
+++ MathJaxEditor/ViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var webviewEditor: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // see : https://stackoverflow.com/questions/24882834/wkwebview-not-loading-local-files-under-ios-8
+        let fileURL = URL(fileURLWithPath: Bundle.main.path(forResource:"mathjax-offline-editor", ofType: "html")!)
+        webviewEditor.loadFileURL(fileURL, allowingReadAccessTo: fileURL)
     }
 
     override var representedObject: Any? {
