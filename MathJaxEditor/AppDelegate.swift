92c021df21a85cd5121eea715be720cfe821d3a0
Orlando, Ding <or.ding@sap.com>
2017-08-31 09:42:37 +0800

    i058959 - for adding support of input of content

--- MathJaxEditor/AppDelegate.swift
+++ MathJaxEditor/AppDelegate.swift
@@ -10,15 +10,28 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    @IBAction func copy(_ sender: Any) {
+    }
+    
+    @IBAction func paste(_ sender: Any) {
+    }
+    
+    @IBAction func selectAll(_ sender: Any) {
+    }
+    
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    // see https://stackoverflow.com/questions/3941960/quit-app-when-nswindow-closes
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
 
 
