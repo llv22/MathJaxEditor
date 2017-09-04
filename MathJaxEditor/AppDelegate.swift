41627af453eb2b0f2085a52eaa30d32890e054b6
Orlando, Ding <or.ding@sap.com>
2017-09-04 18:06:51 +0800

    llv23 - for updates of swift

--- MathJaxEditor/AppDelegate.swift
+++ MathJaxEditor/AppDelegate.swift
@@ -7,19 +7,36 @@
 //
 
 import Cocoa
+import WebKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    // see https://stackoverflow.com/questions/42322146/how-to-paste-an-nsimage-correctly-from-nspasteboard
     @IBAction func copy(_ sender: Any) {
+        if let window = NSApplication.shared().mainWindow {
+            // see https://stackoverflow.com/questions/25708649/downcasting-optionals-in-swift-as-type-or-as-type - downport type
+            if let viewController = window.contentViewController as? ViewController {
+                viewController.copyWebContent()
+            }
+        }
     }
     
     @IBAction func paste(_ sender: Any) {
+        if let window = NSApplication.shared().mainWindow {
+            if let viewController = window.contentViewController as? ViewController {
+                viewController.pasteWebContent()
+            }
+        }
     }
     
     @IBAction func selectAll(_ sender: Any) {
+        if let window = NSApplication.shared().mainWindow {
+            if let viewController = window.contentViewController as? ViewController {
+                viewController.selectAllWebContent()
+            }
+        }
     }
-    
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
--- MathJaxEditor/ViewController.swift
+++ MathJaxEditor/ViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 
 class ViewController: NSViewController {
 
+    // see https://stackoverflow.com/questions/34751860/get-html-from-wkwebview-in-swift
     @IBOutlet weak var webviewEditor: WKWebView!
     
     override func viewDidLoad() {
@@ -27,6 +28,49 @@ class ViewController: NSViewController {
         }
     }
 
-
+    // see https://stackoverflow.com/questions/34751860/get-html-from-wkwebview-in-swift and https://stackoverflow.com/questions/43808304/get-html-element-value-using-wkwebkit-and-swift-3
+    public func copyWebContent() {
+        webviewEditor.evaluateJavaScript("document.getElementById('MathInput').innerHTML") { (html, error) in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let htmlContent = html as? String else {
+                print("MathInput html textarea hasn't been loaded")
+                return
+            }
+            // see : copy to pasteboards https://stackoverflow.com/questions/24670290/how-to-copy-text-to-clipboard-pasteboard-with-swift
+            NSPasteboard.general().clearContents()
+            NSPasteboard.general().setString(htmlContent, forType: NSPasteboardTypeString)
+        }
+    }
+    
+    public func pasteWebContent() {
+        webviewEditor.evaluateJavaScript("document.getElementById('MathInput').innerHTML") { (html, error) in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let htmlContent = html as? String else {
+                print("MathInput html textarea hasn't been loaded")
+                return
+            }
+            // see : copy to pasteboards https://stackoverflow.com/questions/24670290/how-to-copy-text-to-clipboard-pasteboard-with-swift
+            let content = NSPasteboard.general().string(forType: NSPasteboardTypeString)!
+            let newValue = htmlContent + content
+            let executedScript = "document.getElementById('MathInput').innerHTML='" + newValue + "'"
+            self.webviewEditor.evaluateJavaScript(executedScript) { (html, error) in
+                guard error == nil else {
+                    print(error!)
+                    return
+                }
+            }
+        }
+    }
+    
+    public func selectAllWebContent() {
+        print("selectall")
+    }
+    
 }
 
